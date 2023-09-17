5820368959d7e5292fd75be8defd6afa343d69c6
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-17 18:26:05 +0200

    [FEATURE] 02: added tailwind forms to project and config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ const App = () => (
 	<div className="flex flex-col justify-center items-center">
 		<h1 className="mt-2 text-3xl font-bold underline">Welcome to Algorodev Portfolio</h1>
 		<p className="mt-2">I'm full stack developer.</p>
+		<div className="w-1/2">
+			<label htmlFor="test-input">
+				<span>Username</span>
+				<input id="test-input" type="text" className="form-input"/>
+			</label>
+		</div>
 	</div>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindForms from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,6 +17,6 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [tailwindForms],
 }
 
