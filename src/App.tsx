41631f10be301f334e41e8afb4d1f3015016c06f
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-18 18:42:04 +0200

    [FEATURE] 03: app styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import Header from './components/Header/Header.tsx'
+import './App.scss'
+
 const App = () => (
-	<div className="flex flex-col justify-center items-center">
-		<h1 className="mt-2 text-3xl font-bold underline">Welcome to Algorodev Portfolio</h1>
-		<p className="mt-2">I'm full stack developer.</p>
-		<div className="w-1/2">
-			<label htmlFor="test-input">
-				<span>Username</span>
-				<input id="test-input" type="text" className="form-input"/>
-			</label>
-		</div>
-	</div>
+	<main id="top" className="screen">
+		<section className="screen__container screen__container--header">
+			<Header/>
+		</section>
+		<section className="screen__container screen__container--content">
+			<p>Content</p>
+		</section>
+	</main>
 )
 
 export default App
