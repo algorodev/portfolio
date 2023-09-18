b73b86d8f392896440fcb3c8915180c1ced267f0
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-18 18:42:22 +0200

    [FEATURE] 03: header dummy component adn styling

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import './Header.scss'
+
+const Header = () => {
+	const handleContactClick = () => {
+		console.log('Contact!')
+	}
+
+	const handleResumeClick = () => {
+		console.log('Resume!')
+	}
+
+	return (
+		<header className="w-full h-full flex">
+			<section className="pl-8 w-1/2 h-full flex items-center">
+				<a href="#top">
+					<h3 className="text-3xl">LOGO</h3>
+				</a>
+			</section>
+			<section className="pr-8 w-1/2 h-full flex justify-end items-center gap-3">
+				<a href="#education" target="_self" className="nav__links mx-4">
+					Education
+				</a>
+				<a href="#experience" className="nav__links mx-4">
+					<p>Experience</p>
+				</a>
+				<a href="#about" className="nav__links mx-4">
+					<p>About</p>
+				</a>
+				<button id="contact_button" className="nav__button" onClick={handleContactClick}>
+					<label htmlFor="contact_button">Contact</label>
+				</button>
+				<button id="resume_button" className="nav__button" onClick={handleResumeClick}>
+					<label htmlFor="resume_button">Resume</label>
+				</button>
+			</section>
+		</header>
+	)
+}
+
+export default Header
