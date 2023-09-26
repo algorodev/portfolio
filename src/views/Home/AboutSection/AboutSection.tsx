446c97c2feb4fb5ee9707ff9c93534c37edcdda9
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-26 21:22:07 +0200

    [REFACTOR] 06: home styled and about section component

---
+++ src/views/Home/AboutSection/AboutSection.tsx
@@ -0,0 +1,9 @@
+const AboutSection = () => {
+	return (
+		<section className="about__container">
+			<h3>About</h3>
+		</section>
+	)
+}
+
+export default AboutSection
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,14 +1,16 @@
 import EducationSection from './EducationSection/EducationSection.tsx'
+import AboutSection from './AboutSection/AboutSection.tsx'
+import './Home.scss'
 
-const Home = () => {
-
-
-	return (
-		<>
+const Home = () => (
+	<section className="home">
+		<div className="about">
+			<AboutSection />
+		</div>
+		<div className="education">
 			<EducationSection />
-		</>
-	)
-
-}
+		</div>
+	</section>
+)
 
 export default Home
