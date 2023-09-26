03512cd4eafad8e0908f93e980b13d5db1968044
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-26 21:16:49 +0200

    [REFACTOR] 06: header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,30 @@
-import './Header.scss'
 import useHeader from './useHeader.tsx'
+import './Header.scss'
 
 const Header = () => {
 	const { onContactButtonClick, onResumeButtonClick } = useHeader()
 
 	return (
-		<header className="w-full h-full flex">
-			<section className="pl-8 w-1/2 h-full flex items-center">
+		<header className="header">
+			<section className="header__container header__container--left">
 				<a href="#top">
-					<h3 className="text-3xl">LOGO</h3>
+					<h3 className="text-3xl">LOGO</h3> {/* TODO: SET LOGO */}
 				</a>
 			</section>
-			<section className="pr-8 w-1/2 h-full flex justify-end items-center gap-3">
-				<a href="#education" target="_self" className="nav__links mx-4">
+			<section className="header__container header__container--right">
+				<a href="#education" target="_self" className="header__link">
 					Education
 				</a>
-				<a href="#experience" className="nav__links mx-4">
+				<a href="#experience" className="header__link">
 					<p>Experience</p>
 				</a>
-				<a href="#about" className="nav__links mx-4">
+				<a href="#about" className="header__link">
 					<p>About</p>
 				</a>
-				<button id="contact_button" className="nav__button" onClick={onContactButtonClick}>
+				<button id="contact_button" className="header__button" onClick={onContactButtonClick}>
 					<label htmlFor="contact_button">Contact</label>
 				</button>
-				<button id="resume_button" className="nav__button" onClick={onResumeButtonClick}>
+				<button id="resume_button" className="header__button" onClick={onResumeButtonClick}>
 					<label htmlFor="resume_button">Resume</label>
 				</button>
 			</section>
