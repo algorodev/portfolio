81b4b8468d87e7df53027d48218688314f2bc411
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-27 20:08:29 +0200

    [REFACTOR] 07: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Header from './components/Header/Header.tsx'
 import Home from './views/Home/Home.tsx'
 import './App.scss'
+import Footer from './components/Footer/Footer.tsx'
 
 const App = () => (
-	<main id="top" className="screen">
+	<main className="screen">
 		<section className="screen__container screen__container--header">
 			<Header/>
 		</section>
 		<section className="screen__container screen__container--content">
 			<Home />
+			<Footer />
 		</section>
 	</main>
 )
