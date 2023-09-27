8d4528155851dd9f16285d81cb7836dce8be8d22
Alejandro Gonzalez Romero <me@algorodev.com>
2023-09-27 22:10:29 +0200

    [FEATURE] 09: link to download resume

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,36 @@
 import { ArrowDownTrayIcon } from '@heroicons/react/20/solid'
-import useHeader from './useHeader.tsx'
+import cvPdf from '../../assets/algorodev_cv.pdf'
 import './Header.scss'
 
-const Header = () => {
-	const { onResumeButtonClick } = useHeader()
-
-	return (
-		<header className="header">
-			<section className="header__container header__container--left">
-				<h3 className="text-3xl">LOGO</h3> {/* TODO: SET LOGO */}
-			</section>
-			<section className="header__container header__container--right">
-				<a href="#about" className="header__link">
-					<p>About</p>
-				</a>
-				<a href="#contact" className="header__link">
-					<p>Contact</p>
-				</a>
-				<a href="#experience" className="header__link">
-					<p>Experience</p>
-				</a>
-				<a href="#education" target="_self" className="header__link">
-					<p>Education</p>
-				</a>
-				<button id="resume_button" className="header__button" onClick={onResumeButtonClick}>
-					<ArrowDownTrayIcon className="icon" />
-					<label htmlFor="resume_button">Resume</label>
-				</button>
-			</section>
-		</header>
-	)
-}
+const Header = () => (
+	<header className="header">
+		<section className="header__container header__container--left">
+			<h3 className="text-3xl">LOGO</h3> {/* TODO: SET LOGO */}
+		</section>
+		<section className="header__container header__container--right">
+			<a href="#about" className="header__link">
+				<p>About</p>
+			</a>
+			<a href="#contact" className="header__link">
+				<p>Contact</p>
+			</a>
+			<a href="#experience" className="header__link">
+				<p>Experience</p>
+			</a>
+			<a href="#education" target="_self" className="header__link">
+				<p>Education</p>
+			</a>
+			<a id="resume_button"
+			   className="header__button"
+			   href={cvPdf}
+			   download="algorodev_cv"
+			   target="_blank"
+			   rel="noreferrer">
+				<ArrowDownTrayIcon className="icon"/>
+				<label htmlFor="resume_button">Resume</label>
+			</a>
+		</section>
+	</header>
+)
 
 export default Header
--- src/components/Header/__tests__/Header.spec.tsx
+++ src/components/Header/__tests__/Header.spec.tsx
@@ -1,11 +1,6 @@
 import { render, screen } from '@testing-library/react'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import Header from '../Header.tsx'
-
-vi.mock('../useHeader', () => ({
-	__esModule: true,
-	default: () => <div>useHeader</div>
-}))
 
 describe('<Header/>', () => {
 	it('should render component as expected', () => {
