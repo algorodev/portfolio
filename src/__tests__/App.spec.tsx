0a9196312fb56a99b3fbca2eff13f319f28b1bf4
Alejandro Gonzalez Romero <me@algorodev.com>
2024-02-28 16:46:19 +0100

    [FEATURE] removed header and footer components

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -2,16 +2,6 @@ import { render, screen } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 import App from '../App.tsx'
 
-vi.mock('../components/Header/Header', () => ({
-	__esModule: true,
-	default: () => <div>Header</div>
-}))
-
-vi.mock('../components/Footer/Footer', () => ({
-	__esModule: true,
-	default: () => <div>Footer</div>
-}))
-
 vi.mock('../views/Home/Home', () => ({
 	__esModule: true,
 	default: () => <div>Home</div>
@@ -20,9 +10,6 @@ vi.mock('../views/Home/Home', () => ({
 describe('App Component', () => {
 	it('should render app as expected', () => {
 		render(<App />)
-
-		expect(screen.getByText('Header')).toBeInTheDocument()
-		expect(screen.getByText('Home')).toBeInTheDocument()
 		expect(screen.getByText('Footer')).toBeInTheDocument()
 	})
 })
