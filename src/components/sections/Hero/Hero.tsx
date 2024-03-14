2ada574d7d67080d49093ed4f00195d11df41ff6
Alejandro Gonzalez Romero <me@algorodev.com>
2024-03-14 23:35:14 +0000

    [FEATURE] add hero data to cv.json and typed

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -3,63 +3,53 @@ import MailIcon from '../../icons/Mail.tsx'
 import GitHubIcon from '../../icons/GitHub.tsx'
 import LinkedInIcon from '../../icons/LinkedIn.tsx'
 import PhoneIcon from '../../icons/Phone.tsx'
+import { hero } from '../../../data/cv.json'
 import './Hero.scss'
 
-const HeroSection = () => {
-	const name: string = 'Alejandro González Romero'
-	const label: string = 'Full Stack Developer with more than 5 years of experience'
-	const born: string = 'Dublin, Ireland'
-	const email: string = 'me@algorodev.com'
-	const phone: string = '648183931'
-	const linkedInUrl: string = 'https://linkedin.com/in/algorodev'
-	const gitHubUrl: string = 'https://github.com/algorodev'
-	const imagePath: string = '/me.webp'
-
-	return (
-		<section className="container">
-			<div className="info-container">
-				<h1 className="name">{name}</h1>
-				<h2 className="title">{label}</h2>
-				<span className="location">
+const HeroSection = () => (
+	<section className="container">
+		<div className="info-container">
+			<h1 className="name">{hero.name}</h1>
+			<h2 className="title">{hero.label}</h2>
+			<span className="location">
 					<WorldIcon/>
-					{born}
+				{hero.born}
 				</span>
-				<footer className="social-container">
-					<a className="social-button"
-					   href={`mailto:${email}`}
-					   title={`Enviar un correo electrónico a ${name} al correo ${email}`}
-					   target="_blank"
-					   rel="noopener noreferrer">
-						<MailIcon/>
-					</a>
-					<a className="social-button"
-					   href={`tel:${phone}`}
-					   title={`Llamar por teléfono a ${name} al número ${email}`}
-					   target="_blank"
-					   rel="noopener noreferrer">
-						<PhoneIcon/>
-					</a>
-					<a className="social-button"
-					   href={linkedInUrl}
-					   title={`Visitar el perfil de ${name} en LinkedIn`}
-					   target="_blank"
-					   rel="noopener noreferrer">
-						<LinkedInIcon/>
-					</a>
-					<a className="social-button"
-					   href={gitHubUrl}
-					   title={`Visitar el perfil de ${name} en GitHub`}
-					   target="_blank"
-					   rel="noopener noreferrer">
-						<GitHubIcon/>
-					</a>
-				</footer>
-			</div>
-			<figure className="image-container">
-				<img className="image" src={imagePath} alt={name}/>
-			</figure>
-		</section>
-	)
-}
+			<footer className="social-container">
+				<a className="social-button"
+				   href={`mailto:${hero.email}`}
+				   title={`Enviar un correo electrónico a ${hero.name} al correo ${hero.email}`}
+				   target="_blank"
+				   rel="noopener noreferrer">
+					<MailIcon/>
+				</a>
+				<a className="social-button"
+				   href={`tel:${hero.phone}`}
+				   title={`Llamar por teléfono a ${hero.name} al número ${hero.email}`}
+				   target="_blank"
+				   rel="noopener noreferrer">
+					<PhoneIcon/>
+				</a>
+				<a className="social-button"
+				   href={hero.linkedInUrl}
+				   title={`Visitar el perfil de ${hero.name} en LinkedIn`}
+				   target="_blank"
+				   rel="noopener noreferrer">
+					<LinkedInIcon/>
+				</a>
+				<a className="social-button"
+				   href={hero.gitHubUrl}
+				   title={`Visitar el perfil de ${hero.name} en GitHub`}
+				   target="_blank"
+				   rel="noopener noreferrer">
+					<GitHubIcon/>
+				</a>
+			</footer>
+		</div>
+		<figure className="image-container">
+			<img className="image" src={hero.imagePath} alt={hero.name}/>
+		</figure>
+	</section>
+)
 
 export default HeroSection
---
+++ src/cv.d.ts
@@ -0,0 +1,14 @@
+export interface CV {
+	hero: Hero
+}
+
+interface Hero {
+	name: string
+	label: string
+	born: string
+	email: string
+	phone: string
+	linkedInUrl: string
+	gitHubUrl: string
+	imagePath: string
+}
