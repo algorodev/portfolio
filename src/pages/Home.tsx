c4aaa2fd7a3baa1368843988174b0857577a9d9c
Alejandro Gonzalez Romero <me@algorodev.com>
2024-03-14 16:50:26 +0000

    [FEATURE] add section to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import AboutSection from '../components/sections/About/About.tsx'
 import HeroSection from '../components/sections/Hero/Hero.tsx'
+import ProjectsSection from '../components/sections/Projects/Projects.tsx'
 import './Home.scss'
 
 const Home = () => (
@@ -11,6 +12,11 @@ const Home = () => (
 		<section className="section-container">
 			<h2 className="section-title">About me</h2>
 			<AboutSection/>
+		</section>
+
+		<section className="section-container">
+			<h2 className="section-title">Projects</h2>
+			<ProjectsSection/>
 		</section>
 	</main>
 )
