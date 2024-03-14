b3fb51bf74c23f0b37ab4938581aee9c9f748818
Alejandro Gonzalez Romero <me@algorodev.com>
2024-03-14 23:38:52 +0000

    [FEATURE] add about data to cv.json and typed

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -1,14 +1,9 @@
-const AboutSection = () => {
-	const aboutMeText: string = 'Tech-enthusiast with +5 years of experience as a programmer with quick adaptation to' +
-		' new challenges and responsibilities. Experience of working with tight deadlines, under time pressure while' +
-		' ensuring quality and with agile methodologies. I\'m always curious to learn more about technologies and any' +
-		' kind of art, I\'m a passionate about music, films and sports.'
+import { about } from '../../../data/cv.json'
 
-	return (
-		<section>
-			<p>{aboutMeText}</p>
-		</section>
-	)
-}
+const AboutSection = () => (
+	<section>
+		<p>{about.text}</p>
+	</section>
+)
 
 export default AboutSection
