3344446139032caf890b7c8c26a456d91ca03963
Alejandro Gonzalez Romero <me@algorodev.com>
2024-03-14 22:25:16 +0000

    [FEATURE] updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 4567,
+    strictPort: true,
+  },
+  server: {
+    port: 4567,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:4567",
+  },
   test: {
     globals: true,
     environment: 'jsdom',
