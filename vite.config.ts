c6c8099ec09a3cbe45d14a6f115d900a55c7b248
Alejandro Gonzalez Romero <me@algorodev.com>
2024-03-21 16:44:46 +0000

    [FEATURE] removed vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -14,33 +14,5 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:4567",
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-    watch: false,
-    coverage: {
-      provider: 'v8',
-      enabled: true,
-      include: ['src/**'],
-      exclude: [
-        'src/main.tsx',
-        'src/**/*.d.ts',
-        'src/assets/**',
-        'src/types/**',
-      ],
-      all: true,
-      clean: true,
-      cleanOnRerun: true,
-      skipFull: true,
-      preFile: true,
-      reportOnFailure: true,
-      reporter: ['html', 'lcov', 'text-summary'],
-      statements: 80,
-      lines: 80,
-      branches: 80,
-      functions: 80,
-    }
   }
 })
