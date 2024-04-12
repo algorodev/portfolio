aee2e1b99161d212f417704c737745848e6b57f2
Alejandro Gonzalez Romero <me@algorodev.com>
2024-04-12 18:33:14 +0100

    feat: initialized i18n

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home.tsx'
 import './styles/index.scss'
+import './i18n.ts'
 
 const rootElement: HTMLElement = document.getElementById('root')!
 
