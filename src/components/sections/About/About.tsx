55553b20055ca7a4422091cc4de052d2fa5cbf76
Alejandro Gonzalez Romero <me@algorodev.com>
2024-04-12 18:34:15 +0100

    feat: test translated text with params

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -1,9 +1,15 @@
 import { about } from '../../../data/cv.json'
+import { useTranslation } from 'react-i18next'
 
-const AboutSection = () => (
-	<section>
-		<p>{about.text}</p>
-	</section>
-)
+const AboutSection = () => {
+	const { t } = useTranslation()
+
+	return (
+		<section>
+			<p>{t('test', { name: 'Alex' })}</p>
+			<p>{about.text}</p>
+		</section>
+	)
+}
 
 export default AboutSection
