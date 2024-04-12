ddf834fca107f712371525f540d343a6c5f465e0
Alejandro Gonzalez Romero <me@algorodev.com>
2024-04-12 20:07:37 +0100

    feat: added spanish translation json

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,15 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import translationEN from './locales/en/translation.json'
+import translationES from './locales/es/translation.json'
 
 const resources = {
 	en: {
 		translation: translationEN,
 	},
+	es: {
+		translation: translationES,
+	}
 }
 
 i18n.use(initReactI18next).init({
